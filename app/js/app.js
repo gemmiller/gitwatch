27ff5b0e81d20ac3de2a793c82ecc83ff001bd8e
Geoffrey Miller <gmiller@gmiller-ltm.wifi.wl.vail>
2014-06-09 17:03:44 -0500

    Add main dashboard view
    Added the main dash board view and over view template

--- app/js/app.js
+++ app/js/app.js
@@ -2,24 +2,20 @@
  * Created by gmiller on 6/9/14.
  */
 /* App Module */
+    var dashboardApp = angular.module('dashboardApp',['ngRoute','dashboardControllers']);
 
-    var phonecatApp = angular.module('phonecatApp', [
-      'ngRoute',
-          'phonecatControllers'
-        ]);
-
-    phonecatApp.config(['$routeProvider',
-          function($routeProvider) {
-                $routeProvider.
-                  when('/phones', {
-                        templateUrl: 'partials/phone-list.html',
-                        controller: 'PhoneListCtrl'
-                  }).
-      when('/phones/:phoneId', {
-            templateUrl: 'partials/phone-detail.html',
-            controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-            redirectTo: '/phones'
-      });
-  }]);
+dashboardApp.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/overview', {
+                    templateUrl: 'partials/dashboard.html',
+                    controller: 'DashCtrl'
+                }).
+                when('/reports', {
+                    templateUrl: 'partials/reports.html',
+                    controller: 'DashCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/overview'
+                });
+        }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,8 @@
 /**
  * Created by gmiller on 6/9/14.
  */
-var phonecatControllers = angular.module('phonecatControllers', []);
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-      function($scope, $http) {
-            $http.get('phones/phones.json').success(function(data) {
-                  $scope.phones = data;
-            });
-          $scope.orderProp = 'age';
-      }]);
+var dashboardControllers = angular.module('dashboardControllers',[]);
+dashboardControllers.controller('DashCtrl', ['$scope','$http',
+    function($scope, $http) {
+    }]);
 
-    phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-          function($scope, $routeParams) {
-                $scope.phoneId = $routeParams.phoneId;
-}]);
